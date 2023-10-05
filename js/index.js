34bcfabbb46b1306552f8b2426d575198a3f68f5
ToledoPablo <pablotoledo4545@gmail.com>
2023-10-05 16:44:03 -0300

    termine aplicando el conversor de monedas y agregue una parte de cotizacion de monedas

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function preventNonNumericalInput(e) {
   }
 
 
-
+/* Plazo Fijo */
 const monto = document.querySelector('#monto');
 const dias = document.querySelector('#dias');
 const resultado = document.querySelector('.resultado');
@@ -18,7 +18,15 @@ const btn = document.querySelector('.simular_btn');
 const printInt = document.querySelector('.int');
 const date = document.querySelector('.date');
 
+/* Conversor */
 
+const cantidad = document.querySelector('#cantidad');
+const monedaA = document.querySelector('#currency_i');
+const monedaB = document.querySelector('#currency_f');
+const conversorBtn = document.querySelector('.conversor_btn')
+const mostrar = document.querySelector('.conv_final');
+
+// Funcionalidad Plazo Fijo 
 
 function calcularPlazoFijo(montoInicial, plazo) {
 
@@ -38,9 +46,63 @@ function calcularPlazoFijo(montoInicial, plazo) {
     date.innerHTML = `<span>${vencimiento.toLocaleDateString()}</span>`;
 }
 
-
 btn.addEventListener('click', () => {
     if (monto.value != '' && dias.value != '') {
         calcularPlazoFijo(monto.value , dias.value); 
     }
 } );
+
+// Funcionalidad Conversor De Monedas 
+
+function convertir () {
+
+    const dolar = 740;
+    const euro = 402;
+
+    switch (true) {
+        // De Peso a Dolar
+        case monedaA.value == "Peso" && monedaB.value == "Dolar" : 
+            conversion = cantidad.value / dolar;
+            mostrar.innerHTML = `${conversion.toFixed(3)} Dolares`
+            break;
+
+        // De Peso a Euro
+        case monedaA.value == "Peso" && monedaB.value == "Euro":
+            conversion = cantidad.value / euro;
+            mostrar.innerHTML = `${conversion.toFixed(3)} Euros`
+            break;
+
+        // De Dolar a Peso
+        case monedaA.value == "Dolar" && monedaB.value == "Peso":
+            conversion = cantidad.value * dolar;
+            mostrar.innerHTML = `${conversion.toFixed(3)} Pesos`
+            break;
+
+        // De Dolar a Euro
+        case monedaA.value == "Dolar" && monedaB.value == "Euro":
+            conversion = cantidad.value / (dolar/euro);
+            mostrar.innerHTML = `${conversion.toFixed(3)}Euros`
+            break;
+
+        // De Euro a Peso
+        case monedaA.value == "Euro" && monedaB.value == "Peso":
+            conversion = cantidad.value * euro;
+            mostrar.innerHTML = `${conversion.toFixed(3)} Pesos`
+            break;
+
+        // De Euro a Dolar
+        case monedaA.value == "Euro" && monedaB.value == "Dolar":
+            conversion = cantidad.value / (euro / dolar);
+            mostrar.innerHTML = `${conversion.toFixed(3)} Dolares`
+            break;
+
+        default:
+        alert('Los datos que ingreso, no son validos');
+        break;
+    } 
+}
+
+
+conversorBtn.addEventListener('click', () => {
+    convertir()
+})
